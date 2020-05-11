f3b03687c6a77e063325d1a1000dea934d0bc22e
Yan <yanfroes@gmail.com>
2020-05-11 19:35:42 -0300

    some style modifications in quiz, add some questions, add image/gif

--- Quiz.js
+++ Quiz.js
@@ -51,7 +51,7 @@ class Quiz{
     <div class="card">
 		<img
 			class="card-img-top center-things"
-			src="https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/ce2ece60-9b32-11e6-95ab-00163ed833e7/260663710/the-test-fun-for-friends-screenshot.jpg"
+			src="${question.imagegif}"
 			alt="Card image cap"
 		>
 		<div class="card-body">
