2edb7de3b79678f820712bc808771ee2940a3936
Yan <yanfroes@gmail.com>
2020-05-12 20:07:43 -0300

    return quiz.js to old

--- scripts/classes/Quiz.js
+++ scripts/classes/Quiz.js
@@ -1,82 +1,82 @@
 class Quiz{
-    View(self){
-        const Choices = (choices,questionNum) => choices.map((choice, alt) => {
-            return /*html*/`
-                <button
-                    class="center-things button-choice text-center text-button-choices"
-                    onclick="${self}.confirm(${questionNum},${alt})")
-                >${choice}</button>
-            `
-        }).join('')
-    
-        const Buttons = (index) => {
-            let result = ''
-            if(this.index != 0){
-                result += /*html*/`
-                <!-- <button class="center-things button-next" onclick="${this.self}.goBack(${index})">
-                    <span class="text-button">Anterior</span>
-                </button> -->
-                `
-            }
-            return result
-        }
-
-        const Result = () => {
-            let result = RESULTS[this.result]
-            return /*html*/`
-            <div class="card">
-            <img
-                class="card-img-top center-things"
-                src= "images/${result.imagegif}"
-            >
-            <div class="card-body">
-                <h5 class="card-title text-center" style="font-size:24px"><b>Voce é ${result.title}</b></h5>
-                <p class="text-center" style="font-size:22px">${result.text}</p>
-            </div>
-        </div>
-            `
-        }
-
-        const Question = (number, question) => /*html*/`
-            <div class="card">
-                <img
-                    class="card-img-top center-things"
-                    src= "images/${question.imagegif}"
-                >
-                <div class="card-body">
-                    <h5 class="card-title text-center" style="font-size:24px"><b>Pergunta ${number+1}</b></h5>
-                    <p class="text-center" style="font-size:22px">${question.title}</p>
-                    ${Choices(question.choices, number)}
-                    ${Buttons(number)}
-                </div>
-            </div>
-        `
-
-        const Content = () => this.result ? /*html*/`
-            ${Result()}`
-            : /*html*/`
-            ${Question(this.index,this.questions[this.index])}
-        `
-
-        return Content()
-    }
-
     select = (index) => this.update(() => {
         this.selected = index
     })
 
-    confirm = (index,alt) => this.update(() => {
-        let option = this.questions[index].results[alt]
-        console.log('pergunta:',index+1, 'opcao',option)
-        this.answers[index] = option
+    Choices = (choices,questionNum) => choices.map((choice, alt) => {
+        return /*html*/`
+            <button
+                class="center-things button-choice text-center text-button-choices"
+                onclick="${this.self}.confirm(${questionNum},${alt})")
+            >${choice}</button>
+        `
+    }).join('')
 
-        if(this.index < this.questions.length -1){
-            this.index++
+    Buttons = (index) => {
+        let result = ''
+        if(this.index != 0){
+            result += /*html*/`
+            <!-- <button class="center-things button-next" onclick="${this.self}.goBack(${index})">
+                <span class="text-button">Anterior</span>
+            </button> -->
+            `
         }
-        else{
-            this.result = this.getResult()
-        }
-    })
+        return result
+    }
+
+    Result = () => {
+        let result = RESULTS[this.result]
+        return /*html*/`
+        <div class="card">
+		<img
+			class="card-img-top center-things"
+			src= "${result.imagegif}"
+		>
+		<div class="card-body">
+		    <h5 class="card-title text-center" style="font-size:24px"><b>Voce é ${result.title}</b></h5>
+			<p class="text-center" style="font-size:22px">${result.text}</p>
+      <a href="http://groupjungle.site/quiz.html"><button class="center-things button-next">
+          <span class="text-button">Refazer Quiz</span>
+      </button></a>
+		</div>
+	</div>
+        `
+    }
+
+    Question = (number, question) => /*html*/`
+
+    <div class="card">
+		<img
+			class="card-img-top center-things"
+			src= "${question.imagegif}"
+		>
+		<div class="card-body">
+		    <h5 class="card-title text-center" style="font-size:24px"><b>Pergunta ${number+1}</b></h5>
+			<p class="text-center" style="font-size:22px">${question.title}</p>
+            ${this.Choices(question.choices, number)}
+            ${this.Buttons(number)}
+		</div>
+	</div>
+    `
+
+    Content = () => this.result ? /*html*/`
+        ${this.Result()}`
+        : /*html*/`
+        ${this.Question(this.index,this.questions[this.index])}
+        `
+
+    update(action = () => {}){
+        action()
+        this.render()
+    }
+
+    constructor(self){
+        this.questions = QUESTIONS
+        this.self = self
+        this.index = 0
+        this.answers = []
+        this.render()
+    }
 
     getResult = () => {
         let each = {}
@@ -94,17 +94,25 @@ class Quiz{
         return parseInt(result)
     }
 
-    constructor(self){
-        this.questions = QUESTIONS
-        this.self = self
-        this.index = 0
-        this.answers = []
-        this.update = (action = () => {}) => {
-            action()
-            document.querySelector('#quiz')
-                .innerHTML = this.View(this.self)
+    confirm = (index,alt) => this.update(() => {
+        let option = this.questions[index].results[alt]
+        console.log('pergunta:',index+1, 'opcao',option)
+        this.answers[index] = option
+
+        if(this.index < this.questions.length -1){
+            this.index++
         }
-        this.update()
+        else{
+            this.result = this.getResult()
+        }
+    })
+
+    goBack = (index) => this.update(() => {
+        this.index--
+    })
+
+    render(){
+        document.querySelector('#quiz').innerHTML = this.Content()
     }
 }
 
