589a762b0cf7e142a9ad7662c0d81003c0b47634
Yan <yanfroes@gmail.com>
2020-05-11 23:25:46 -0300

    update questions

--- questions.js
+++ questions.js
@@ -2,56 +2,56 @@ QUESTIONS = []
 
 QUESTIONS.push({
     title: "Indicando um produto:",
-    imagegif: "https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/ce2ece60-9b32-11e6-95ab-00163ed833e7/260663710/the-test-fun-for-friends-screenshot.jpg",
+    imagegif: "",
     choices: ["É criativo na hora de indicar a alguém", "Repete a mesma fala de quem te indicou ", "Indica um produto e posta usando outro", "Não indica nada nunca, só pagando "],
-    results: ["Larissa", "Manoela", "Maísa", "Justin"]
+    results: [4, 3,2 ,1 ]
 })
 
 QUESTIONS.push({
     title: "Quando grava um story começa com:",
-    imagegif: "gif1.gif",
+    imagegif: "",
     choices: ["Geeente", "Falaaa Galeraaa", "Muita gente tá me pedindo…", "Não falo nada, só posto foto"],
-    results: ["Larissa", "Manoela", "Maísa", "Justin"]
+    results: [3,4 ,2 ,1 ]
 })
 
 QUESTIONS.push({
     title: "Você é do tipo que:",
-    imagegif: "https://media.giphy.com/media/FYqf889lXd9Ru/source.gif",
+    imagegif: "",
     choices: ["Não passa um dia sem postar nada ", "Gosta de dar uma sumida", "Posta uma quantidade certa de stories por dia", "Posta quase 50 stories durante o dia, seus seguidores que lutem"],
-    results: ["Larissa", "Manoela", "Maísa", "Justin"]
+    results: [2, 1, 3, 4]
 })
 
 QUESTIONS.push({
     title: "Conteúdo:",
     imagegif: "https://media.giphy.com/media/eh7W3dJGvRa4FymAPf/source.gif",
-    choices: ["Você ama compartilhar novidades de make e cuidados com a pele", "Você gosta de polêmicas", "Você compartilha receitas", "Você ensina como fazer exercícios "],
-    results: ["Larissa", "Manoela", "Maísa", "Justin"]
+    choices: ["Você ama compartilhar novidades de make e cuidados com a pele", "Você gosta de polêmicas", "Você ama compartilhar tudo sobre roupas e moda ", "Você não tem conteúdo específico "],
+    results: [3, 2, 4,1 ]
 })
 
 QUESTIONS.push({
     title: "O seu feed:",
     imagegif: "https://media.giphy.com/media/zpIADSd2bkC9G/source.gif",
     choices: ["Tem trabalho e sua rotina também", "Tem só cliques por fotógrafos ", "Muita natureza e reflexões", "Só usando produtos de grife "],
-    results: ["Larissa", "Manoela", "Maísa", "Justin"]
+    results: [3, 4,1 ,2 ]
 })
 
 QUESTIONS.push({
     title: "Qual é o seu tipo de feed? ",
     imagegif: "https://media.giphy.com/media/VdJVBpxwoFRfP9jVNE/source.gif",
     choices: ["Trabalho com paleta de cores ", "As fotos precisam estar sequenciadas em 3-3", "Eu não ligo pra isso, posto a foto do momento no qual eu to vivendo ", "Eu só vivo de #TBT"],
-    results: ["Larissa", "Manoela", "Maísa", "Justin"]
+    results: [4, 1, 2, 3]
 })
 
 QUESTIONS.push({
     title: "Qual meme mais representa você?",
     imagegif: "https://media.giphy.com/media/VdJVBpxwoFRfP9jVNE/source.gif",
     choices: ["a", "b", "c", "d"],
-    results: ["Larissa", "Manoela", "Maísa", "Justin"]
+    results: [1, 2, 3, 4]
 })
 
 QUESTIONS.push({
     title: "Qual challenge você se desafiaria fazer?",
     imagegif: "",
     choices: ["1", "2", "3", "4"],
-    results: ["Larissa", "Manoela", "Maísa", "Justin"]
+    results: [1, 2, 3, 4]
 })
