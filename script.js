44daf020f1e4477c09c56c95d13078c49c9cd4c4
Yan <yanfroes@gmail.com>
2020-05-11 22:15:39 -0300

    add first page georgia style

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+
+
+
+
 /*
 
 (function() {
